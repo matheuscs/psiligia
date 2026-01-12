740cbeefe33c06dc2b2740bfb3a62a695a86ef9c
Matheus Silva <matheuscarvas@gmail.com>
2026-01-12 17:22:22 -0300

    adding map from google business

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,22 @@
 import { InstagramLogo, WhatsappLogo } from 'phosphor-react'
 import './Home.css'
 
+const MapaConsultorio = () => {
+  return (
+    <div className="mapa-container" style={{ marginTop: '2rem', marginBottom: '2rem' }}>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d229.39796274232282!2d-47.20513738101647!3d-23.08364266012798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8b532af2af24b%3A0x6ee7b5e032a6e3af!2sPsic%C3%B3loga%20L%C3%ADgia%20Rangel!5e0!3m2!1spt-BR!2sbr!4v1768247646110!5m2!1spt-BR!2sbr"
+        width="100%"
+        height="400"
+        style={{ border: 0, borderRadius: '12px' }} // Adicionei um arredondamento para ficar mais moderno
+        allowFullScreen={true}
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
+
 export function Home() {
   return(
     <div>
@@ -21,13 +37,14 @@ export function Home() {
             </a>
           </div>
         </div>
-        </div>
+      </div>
       <div className='content'>
         <div className='introduction'>
           <p>Olá! Eu sou a Lígia Rangel, psicóloga, e estou pronta para te ajudar! </p>
           <p>Vamos marcar uma consulta?</p>
-          <p>Entre em contato pelas minhas redes socias clicando nos links acima!</p>
+          <p>Entre em contato pelas minhas redes sociais clicando nos links acima!</p>
         </div>
+        <MapaConsultorio />
       </div>
       <footer>
         <p>© 2025 Lígia Rangel</p>
